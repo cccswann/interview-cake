3d335c44d7c7484c424b58c2bcd4038f78fcce26
Ciara <cccswann@gmail.com>
2021-01-03 18:09:18 -0800

    Add function

--- logarithms.py
+++ logarithms.py
@@ -8,3 +8,14 @@ def binary_search(target, nums):
     # (we *don't* mean the last index)
 
     floor_index = -1
+    ceiling_index = len(nums)
+
+    # If there isn't at least 1 index between floor and ceiling,
+    # we've run out of guesses and the number must not be present
+
+    while floor_index + 1 < ceiling_index:
+        # Find the index ~halfway between the floor and ceiling
+        # we use integer division, so we'll never get a "half index"
+        distance = ceiling_index - floor_index
+        half_distance = distance // 2
+        guess_index = floor_index + half_distance
