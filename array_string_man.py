5821332df2f0684b09a4efc6b0ffae3ae792b6ee
Ciara <cccswann@gmail.com>
2021-01-11 19:13:38 -0800

    Add to array slicing

--- array_string_man.py
+++ array_string_man.py
@@ -2,3 +2,6 @@
 my_list[start_index:end_index]
 my_list[start_index:] # equivalent of above
 tail_of_list = my_list[1:]
+
+# save the result to a variable
+return my_list[1:] # just spent O(n) time and space!
